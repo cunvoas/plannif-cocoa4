f2fad27811c0bcddfa1319960e8a06c728ad4a6e
cunvoas <christophe.unvoas@gmail.com>
2017-04-04 10:08:01 +0200

    Initial commit

---
+++ src/main/java/com/github/cunvoas/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.github.cunvoas;
+
+/**
+ * @author UNVOAS
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/com/github/cunvoas/TestMain.java
@@ -0,0 +1,18 @@
+package com.github.cunvoas;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestMain {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		Assert.fail("Not yet implemented");
+	}
+
+}
